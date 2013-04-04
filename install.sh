81e3631a4da259cdf95aa62b95579876acef4c53
Gael Utard <gael.utard@makina-corpus.com>
2013-04-04 15:50:36 +0200

    Fix db creation in install script

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ fi
 if ! sudo -u postgres psql -tAl | grep "^${db_name}"; then
         sudo -u postgres psql -c "CREATE DATABASE ${db_name} OWNER ${db_user};"
         sudo -u postgres psql -f /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql -d ${db_name}
-        sudo -u postgres psql -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql -d ${db_user}
+        sudo -u postgres psql -f /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql -d ${db_name}
         sudo -u postgres psql ${db_name} -c "ALTER TABLE geometry_columns OWNER TO ${db_user};"
         sudo -u postgres psql ${db_name} -c "ALTER TABLE spatial_ref_sys OWNER TO ${db_user};"
 fi
