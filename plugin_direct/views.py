82dcd129d3f8c63f51265b43eb0b7b2ee56ce917
Gael Utard <gael.utard@makina-corpus.com>
2013-05-06 15:31:20 +0200

    Add more "En direct" content

--- plugin_direct/views.py
+++ plugin_direct/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.template import RequestContext
 from ionyweb.website.rendering.utils import render_view
+from coop_local.models import Organization
+from coop_local.models.local_models import ORGANIZATION_STATUSES
+from ionyweb.page_app.page_blog.models import Entry
 
 # from ionyweb.website.rendering.medias import CSSMedia, JSMedia, JSAdminMedia
 MEDIAS = (
@@ -12,8 +15,11 @@ MEDIAS = (
     # JSAdminMedia('plugin_direct_actions.js'),
     )
 
-def index_view(request, plugin):    
+def index_view(request, plugin):
+    providers = Organization.objects.filter(is_provider=True, status=ORGANIZATION_STATUSES.VALIDATED).order_by('-validation')[:6]
+    customers = Organization.objects.filter(is_customer=True, status=ORGANIZATION_STATUSES.VALIDATED).order_by('-validation')[:6]
+    actus = Entry.objects.filter(status=Entry.STATUS_ONLINE).order_by('-publication_date')[:6]
     return render_view('plugin_direct/index.html',
-                       {'object': plugin},
+                       {'object': plugin, 'providers': providers, 'customers': customers, 'actus': actus},
                        MEDIAS,
                        context_instance=RequestContext(request))
