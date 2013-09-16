0afaed9a3996465db106deea04ece413eaf8a766
Antoine Cezar <antoine.cezar@makina-corpus.com>
2013-09-16 16:30:19 +0200

    Fix domains in pg_restore

--- pg_restore.sh
+++ pg_restore.sh
@@ -7,7 +7,14 @@ if [ -z "${DB_NAME}" ]; then
     echo "ERROR: DB_NAME is not defined"
     exit 1
 fi
+if [ -z "${DOMAIN}" ]; then
+    echo "ERROR: DOMAIN is not defined"
+    exit 1
+fi
 
 echo "DROP SCHEMA public CASCADE;" | sudo -u postgres psql $DB_NAME
 echo "CREATE SCHEMA public AUTHORIZATION ${DB_USER};" | sudo -u postgres psql $DB_NAME
 pg_restore $1 | sudo -u postgres psql $DB_NAME
+
+sudo -u postgres psql -d ${DB_NAME} -c "UPDATE django_site SET name='localhost', domain='${DOMAIN}' WHERE id=1;"
+sudo -u postgres psql -d ${DB_NAME} -c "UPDATE multisite_alias SET domain='${DOMAIN}' WHERE id=1;"
