6d650da054bd67ed89489b491d47ab02a1d6eb43
Gael Utard <gael.utard@makina-corpus.com>
2013-09-18 16:18:58 +0200

    Bug #248: Prototype of command to send a mail with login info

--- coop_local/management/commands/mailing.py
+++ coop_local/management/commands/mailing.py
@@ -9,12 +9,18 @@ from BeautifulSoup import BeautifulSoup
 from django.conf import settings
 from django.template import loader, Context
 from django.core.mail import EmailMultiAlternatives
+from django.contrib.auth.models import User
+from ionyweb.plugin_app.plugin_contact.models import Plugin_Contact
 
 from email.MIMEImage import MIMEImage
 import email.Charset
 
 from django.core.management.base import BaseCommand, CommandError
 from coop_local.models import Organization
+import unicodedata
+import re
+import random
+import string
 
 CHARSET = 'utf-8'
 
@@ -25,7 +31,7 @@ named = lambda email, name: ('%s <%s>' % (email, name)) if name else email
 def image_finder(tag):
     return (tag.name == u'img' or
             tag.name == u'table' and tag.has_key('background'))
-    
+
 def render(context, template):
     if template:
         t = loader.get_template(template)
@@ -38,14 +44,14 @@ def send_html_mail(subject, recipient, message, template='',
     """
     If you want to use Django template system:
        use `message` and define `template`.
-    
+
     If you want to use images in html message, no problem,
-    it will attach automatically found files in html template. 
+    it will attach automatically found files in html template.
     (image paths are relative to PROJECT_PATH)
     """
-    
+
     html = render(message, template)
-    
+
     # Image processing, replace the current image urls with attached images.
     soup = BeautifulSoup(html)
     images = []
@@ -60,23 +66,23 @@ def send_html_mail(subject, recipient, message, template='',
             continue
         added_images.append(tag[name])
         images.append((tag[name], 'img%d' % index))
-        tag[name] = 'cid:img%d' % index    
+        tag[name] = 'cid:img%d' % index
     html = str(soup)
-    
+
     msg = EmailMultiAlternatives(
         subject=subject,
         body=html,
         to=[named(recipient, recipient_name)],
         from_email=named(sender, sender_name),
     )
-    
+
     for filename, file_id in images:
         image_file = open(settings.PROJECT_PATH + filename, 'rb')
         msg_image = MIMEImage(image_file.read())
         image_file.close()
         msg_image.add_header('Content-ID', '<%s>' % file_id)
         msg.attach(msg_image)
-    
+
     msg.content_subtype = 'html'
     msg.mixed_subtype = 'related'
     msg.send()
@@ -87,6 +93,33 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        #for org in Organization.objects.all():
-        send_html_mail(u'Accédez à votre fiche dans achetons-solidaires-paca.com', 'gut', {},
-            template='mailing.html', sender='achats.responsables@apeas.fr')
+        sender = Plugin_Contact.objects.all()[0].email
+ 
+        for org in Organization.objects.filter(is_provider=True, title='tata'):
+            if User.objects.filter(person__engagements__org_admin=True, person__engagements__organization=org).exists():
+                continue
+            member = org.engagement_set.filter(org_admin=True)[0]
+            person = member.person
+            email = member.email or org.contacts.filter(contact_medium__label='Email')[0].content
+            username = person.first_name + '.' + person.last_name
+            username = unicodedata.normalize('NFKD', unicode(username))
+            username = username.encode('ASCII', 'ignore')
+            username = username.lower()
+            username = username.replace(' ', '_')
+            username = re.sub(r'[^a-z_\.-]', '-', username)
+            password = ''.join([random.choice(string.digits + string.letters) for i in range(0, 6)]).lower()
+            user = User(
+                first_name=person.first_name,
+                last_name=person.last_name,
+                email=email,
+                username=username
+            )
+            user.set_password(password)
+            print 'user:', user, ',', user.username, ',', password
+            user.save()
+            person.user = user
+            person.save()
+            print 'send mail to %s' % email
+            send_html_mail(u'Accédez à votre fiche dans achetons-solidaires-paca.com', email,
+                {'username': username, 'password': password, 'sender': sender},
+                template='mailing.html', sender=sender)
