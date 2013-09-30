8d51c1c311197620dfb73b96204743fb80cd7eb9
Antoine Cezar <antoine.cezar@makina-corpus.com>
2013-09-30 16:42:54 +0200

    Add missing libmysqlclient-dev dependency

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source .env
 if [ -z "${DB_USER}" ]; then
     echo "ERROR: DB_USER is not defined"
     exit 1
@@ -15,6 +16,7 @@ sudo apt-get -y update
 sudo apt-get -y install make git mercurial
 sudo apt-get -y install python-dev python-virtualenv
 sudo apt-get -y install postgresql postgresql-9.1-postgis postgresql-server-dev-9.1
+sudo apt-get -y libmysqlclient-dev
 sudo apt-get -y install gdal-bin libgeoip1
 sudo apt-get -y install libjpeg8-dev
 sudo apt-get -y install openoffice-writer python-uno
