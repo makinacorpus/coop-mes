a0493bcda34be136f3c1ef67cdb4bd80a9546fa0
Gael Utard <gael.utard@makina-corpus.com>
2013-11-19 12:04:33 +0100

    Bug #626 : ionyweb plugin translation

--- plugin_direct/__init__.py
+++ plugin_direct/__init__.py
@@ -2,9 +2,9 @@
 from django.utils.translation import ugettext as _
 
 PLUGIN_INFO = {
-    'NAME': _(u"direct"),
+    'NAME': u"En direct",
     'CATEGORY': 'text',
     'VERSION': "1.0",
-    'SHORT_DESCRIPTION': _(u"Please edit plugin_direct./__init__.py"),
-    'DESCRIPTION': _(u"Please edit plugin_direct./__init__.py"),
+    'SHORT_DESCRIPTION': u"Liste des éléments à afficher en direct.",
+    'DESCRIPTION': u"Liste des éléments à afficher en direct.",
 }
--- plugin_last_news/__init__.py
+++ plugin_last_news/__init__.py
@@ -2,9 +2,9 @@
 from django.utils.translation import ugettext as _
 
 PLUGIN_INFO = {
-    'NAME': _(u"last_news"),
+    'NAME': u"A la une",
     'CATEGORY': 'text',
     'VERSION': "1.0",
-    'SHORT_DESCRIPTION': _(u"Please edit plugin_last_news./__init__.py"),
-    'DESCRIPTION': _(u"Please edit plugin_last_news./__init__.py"),
+    'SHORT_DESCRIPTION': u"Carrousel des éléments dont la case \"A la une\" est cochée.",
+    'DESCRIPTION': u"Carrousel des éléments dont la case \"A la une\" est cochée.",
 }
