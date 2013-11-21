8e5b8a60eabd997afee3593fb95daa8c2153c4e6
Gael Utard <gael.utard@makina-corpus.com>
2013-11-21 16:59:11 +0100

    Bug #661: resources space layout

--- page_account/forms.py
+++ page_account/forms.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import floppyforms as forms
+from ionyweb.forms import ModuloModelForm
+from models import PageApp_Account
+from ionyweb.widgets import TinyMCELargeTable
+
 from django.contrib.auth.forms import AuthenticationForm as BaseAuthenticationForm
+
+
+class PageApp_AccountForm(ModuloModelForm):
+
+    class Meta:
+        model = PageApp_Account
 
 
 class AuthenticationForm(BaseAuthenticationForm):
