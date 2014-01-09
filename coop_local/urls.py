c26c52c2f26880b7612af4af36b9237d9ede37c7
Gael Utard <gael.utard@makina-corpus.com>
2014-01-09 15:17:49 +0100

    Allow to upload files without being a staff member

--- coop_local/urls.py
+++ coop_local/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url
 from page_directory.views import add_target_view
+from mce_filebrowser import views
 
 # # https://code.djangoproject.com/ticket/10405#comment:11
 # from django.db.models.loading import cache as model_cache
@@ -16,7 +17,28 @@ urlpatterns = patterns('',
     ('^$', 'django.views.generic.simple.redirect_to', {'url': '/admin', 'permanent': False}),
     url(r'^ajouter_organisation/$', add_target_view, name='add_target'),
     #url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^mce_filebrowser/', include('mce_filebrowser.urls')),
+    #url(r'^mce_filebrowser/', include('mce_filebrowser.urls')),
+    # .__closure__[0].cell_contents -> strips @staff_member_required decorator
+    url(r'^mce_filebrowser/image/$', 
+        views.filebrowser.__closure__[0].cell_contents, 
+        {'file_type': 'img'},
+        name='mce-filebrowser-images'
+    ),
+    url(r'^mce_filebrowser/file/$', 
+        views.filebrowser.__closure__[0].cell_contents, 
+        {'file_type': 'doc'},
+        name='mce-filebrowser-documents'
+    ),
+    url(r'^mce_filebrowser/image/remove/(?P<item_id>\d+)/$', 
+        views.filebrowser_remove_file.__closure__[0].cell_contents, 
+        {'file_type': 'img'},
+        name='mce-filebrowser-remove-image'
+    ),
+    url(r'^mce_filebrowser/file/remove/(?P<item_id>\d+)/$', 
+        views.filebrowser_remove_file.__closure__[0].cell_contents, 
+        {'file_type': 'doc'},
+        name='mce-filebrowser-remove-document'
+    )
 )
 
 
