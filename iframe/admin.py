6e3914ee0ecffc34fb4af4de9fd43d250bb24886
Gael Utard <gael.utard@makina-corpus.com>
2014-01-15 10:36:06 +0100

    Show HTML code to insert to show iframe

--- iframe/admin.py
+++ iframe/admin.py
@@ -2,5 +2,19 @@
 
 from django.contrib import admin
 from .models import IFrame
+from django.contrib.sites.models import Site
 
-admin.site.register(IFrame)
+
+class IFrameAdmin(admin.ModelAdmin):
+    change_form_template = 'admin/iframe/iframe/tabbed_change_form.html'
+    list_display = ('id', 'domain', 'title')
+    list_display_links = ('domain', )
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        extra_context['domain'] = site = Site.objects.get_current().domain
+        return super(IFrameAdmin, self).change_view(request, object_id,
+            form_url, extra_context=extra_context)
+
+
+admin.site.register(IFrame, IFrameAdmin)
